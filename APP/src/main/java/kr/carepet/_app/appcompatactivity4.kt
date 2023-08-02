5e02de0681203f2e678434ab0eb1b1aba17a57e8
care-pet-isyuun <isyuun@care-biz.co.kr>
2023-08-02 17:08:03 +0900

    :APP: alert 메서드 추가 :SDK: 변경: 33 -> 34

--- APP/src/main/java/kr/carepet/_app/appcompatactivity4.kt
+++ APP/src/main/java/kr/carepet/_app/appcompatactivity4.kt
@@ -21,10 +21,11 @@ import androidx.appcompat.app.AlertDialog
  * @description :
  */
 open class appcompatactivity4 : appcompatactivity3() {
-    private lateinit var alertDialog: AlertDialog
+    private var alert: AlertDialog? = null
 
-    fun dialog(title: CharSequence, message: CharSequence, positive: OnClickListener? = null, negative: OnClickListener? = null, neutral: OnClickListener? = null) {
-        alertDialog = this.let {
+    private fun dialog(title: CharSequence, message: CharSequence, positive: OnClickListener? = null, negative: OnClickListener? = null, neutral: OnClickListener? = null) {
+        alert?.dismiss()
+        alert = this.let {
             val builder = AlertDialog.Builder(it)
             builder.apply {
                 setTitle(title)
@@ -33,11 +34,24 @@ open class appcompatactivity4 : appcompatactivity3() {
                 negative?.let { setNegativeButton(android.R.string.cancel, negative) }
                 neutral?.let { setNeutralButton(android.R.string.ok, neutral) }
             }
+
             // Set other dialog properties
             //TODO
 
             // Create the AlertDialog
             builder.create()
         }
+    }
+
+    fun ok(message: CharSequence, positive: OnClickListener) {
+        dialog(getString(android.R.string.ok), message, positive)
+    }
+
+    fun cancel(message: CharSequence, negative: OnClickListener) {
+        dialog(getString(android.R.string.ok), message, null, negative)
+    }
+
+    fun show(message: CharSequence, neutral: OnClickListener) {
+        dialog(getString(android.R.string.ok), message, null, null, neutral)
     }
 }
