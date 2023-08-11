01a908b126422a525f26a059c3b44d521a32364d
care-pet-isyuun <isyuun@care-biz.co.kr>
2023-08-11 15:17:55 +0900

    전체: 3. 버전 카탈로그 마이그레이션 (libs.versions.toml)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    //id("com.android.application") version "8.1.0" apply false
+    alias(libs.plugins.com.android.application) apply false
+    //id("com.android.library") version "8.1.0" apply false
+    alias(libs.plugins.com.android.library) apply false
+    //id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    //id("com.google.gms.google-services") version "4.3.15" apply false
+    alias(libs.plugins.com.google.gms.google.services) apply false
 }
