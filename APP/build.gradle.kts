dae2361d25f8b8ef438886a99c091e7d3a788671
isyuun-carepet <isyuun@care-biz.co.kr>
2023-08-21 13:03:35 +0900

    APP: build.gradle.kts : 버전 카달로그 적용 and: gradle.properties : android.nonFinalResIds=false

--- APP/build.gradle.kts
+++ APP/build.gradle.kts
@@ -52,14 +52,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation(libs.junit4)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
 }
