62c473acf87e81d7cc462c9194c9f5883145837d
isyuun-carepet <isyuun@care-biz.co.kr>
2023-10-06 16:48:30 +0900

    Navi: GPS.MAP 추가 MyApplication : kr.carepet.map.app.MapApplication MapActivity : kr.carepet.map.app.MapActivity

--- Navi/src/main/java/kr/carepet/app/navi/MyApplication.kt
+++ Navi/src/main/java/kr/carepet/app/navi/MyApplication.kt
@@ -1,16 +1,14 @@
 package kr.carepet.app.navi
 
-import android.app.Application
 import android.content.ContentValues.TAG
 import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.util.Utility
-import kr.carepet.singleton.MySharedPreference
 
-class MyApplication : Application() {
+class MyApplication : kr.carepet.map.app.MapApplication() {
     override fun onCreate() {
         super.onCreate()
-        Log.d("Log","application 진입")
+        Log.d("Log", "application 진입")
 
         KakaoSdk.init(this, "55dcd3a241d864c2399f6ca1e466d57d")
         kr.carepet.singleton.MySharedPreference.init(this)
