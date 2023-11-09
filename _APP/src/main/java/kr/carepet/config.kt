0e2a4cbe3f08c5d156da17d45e0c72d1d7beb2cf
isyuun-care-pet <isyuun@care-biz.co.kr>
2023-11-09 13:00:09 +0900

    펫팁: 소스정리 임시저장 _COPY:카피라잇 _HEAD:주석헤더

--- _APP/src/main/java/kr/carepet/config.kt
+++ _APP/src/main/java/kr/carepet/config.kt
@@ -1,4 +1,23 @@
+/*
+ * Copyright (c) 2023. PetTip All right reserved.
+ * This software is the proprietary information of Care Pet.
+ *
+ * Revision History
+ *   Author                         Date          Description
+ *   --------------------------     ----------    ----------------------------------------
+ *   isyuun                         2023.11.9
+ *
+ */
+
 package kr.carepet
 
+/**
+ * @Project     : PetTip-Android
+ * @FileName    : config
+ * @Date        : 2023-11-09
+ * @author      : isyuun
+ * @description : kr.carepet
+ * @see kr.carepet.config
+ */
 val DEBUG = kr.carepet.BuildConfig.DEBUG
 //val DEBUG = false
