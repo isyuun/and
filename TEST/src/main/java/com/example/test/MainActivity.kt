3c110ca218fd7a8af615cd2e9505e2ebefd67bb5
isyuun-care-pet <isyuun@care-biz.co.kr>
2023-12-22 18:32:17 +0900

    _APP/GPS/MAP/TEST: 테마.테마.테마...라이트/다크.테마

--- TEST/src/main/java/com/example/test/MainActivity.kt
+++ TEST/src/main/java/com/example/test/MainActivity.kt
@@ -1,25 +1,72 @@
+/*
+ * Copyright (c) 2023. PetTip All right reserved.
+ * This software is the proprietary information of Care Biz.
+ *
+ * Revision History
+ *   Author                         Date          Description
+ *   --------------------------     ----------    ----------------------------------------
+ *   isyuun                         2023.12.22
+ *
+ */
+
 package com.example.test
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.test.ui.theme.AndTheme
 
+/**
+ * @Project     : PetTip-Android
+ * @FileName    : MainActivity
+ * @Date        : 2023-12-22
+ * @author      : isyuun
+ * @description : com.example.test
+ * @see com.example.test.MainActivity
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface {
+                    AppContent()
                 }
             }
         }
@@ -27,17 +74,230 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun AppContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        ColorChip(label = "primary", color = MaterialTheme.colorScheme.primary, text = MaterialTheme.colorScheme.onPrimary)
+        ColorChip(label = "onPrimary", color = MaterialTheme.colorScheme.onPrimary, text = MaterialTheme.colorScheme.primary)
+        ColorChip(label = "primaryContainer", color = MaterialTheme.colorScheme.primaryContainer, text = MaterialTheme.colorScheme.onPrimaryContainer)
+        ColorChip(label = "onPrimaryContainer", color = MaterialTheme.colorScheme.onPrimaryContainer, text = MaterialTheme.colorScheme.primaryContainer)
+        ColorChip(label = "inversePrimary", color = MaterialTheme.colorScheme.inversePrimary, text = MaterialTheme.colorScheme.primary)
+        ColorChip(label = "secondary", color = MaterialTheme.colorScheme.secondary, text = MaterialTheme.colorScheme.onSecondary)
+        ColorChip(label = "onSecondary", color = MaterialTheme.colorScheme.onSecondary, text = MaterialTheme.colorScheme.secondary)
+        ColorChip(label = "secondaryContainer", color = MaterialTheme.colorScheme.secondaryContainer, text = MaterialTheme.colorScheme.onSecondaryContainer)
+        ColorChip(label = "onSecondaryContainer", color = MaterialTheme.colorScheme.onSecondaryContainer, text = MaterialTheme.colorScheme.secondaryContainer)
+        ColorChip(label = "tertiary", color = MaterialTheme.colorScheme.tertiary, text = MaterialTheme.colorScheme.onTertiary)
+        ColorChip(label = "onTertiary", color = MaterialTheme.colorScheme.onTertiary, text = MaterialTheme.colorScheme.tertiary)
+        ColorChip(label = "tertiaryContainer", color = MaterialTheme.colorScheme.tertiaryContainer, text = MaterialTheme.colorScheme.onTertiaryContainer)
+        ColorChip(label = "onTertiaryContainer", color = MaterialTheme.colorScheme.onTertiaryContainer, text = MaterialTheme.colorScheme.tertiaryContainer)
+        ColorChip(label = "background", color = MaterialTheme.colorScheme.background, text = MaterialTheme.colorScheme.onBackground)
+        ColorChip(label = "onBackground", color = MaterialTheme.colorScheme.onBackground, text = MaterialTheme.colorScheme.background)
+        ColorChip(label = "surface", color = MaterialTheme.colorScheme.surface, text = MaterialTheme.colorScheme.onSurface)
+        ColorChip(label = "onSurface", color = MaterialTheme.colorScheme.onSurface, text = MaterialTheme.colorScheme.surface)
+        ColorChip(label = "surfaceVariant", color = MaterialTheme.colorScheme.surfaceVariant, text = MaterialTheme.colorScheme.onSurfaceVariant)
+        ColorChip(label = "onSurfaceVariant", color = MaterialTheme.colorScheme.onSurfaceVariant, text = MaterialTheme.colorScheme.surfaceVariant)
+        ColorChip(label = "surfaceTint", color = MaterialTheme.colorScheme.surfaceTint)
+        ColorChip(label = "inverseSurface", color = MaterialTheme.colorScheme.inverseSurface, text = MaterialTheme.colorScheme.inverseOnSurface)
+        ColorChip(label = "inverseOnSurface", color = MaterialTheme.colorScheme.inverseOnSurface, text = MaterialTheme.colorScheme.inverseSurface)
+        ColorChip(label = "error", color = MaterialTheme.colorScheme.error, text = MaterialTheme.colorScheme.onError)
+        ColorChip(label = "onError", color = MaterialTheme.colorScheme.onError, text = MaterialTheme.colorScheme.error)
+        ColorChip(label = "errorContainer", color = MaterialTheme.colorScheme.errorContainer, text = MaterialTheme.colorScheme.onErrorContainer)
+        ColorChip(label = "onErrorContainer", color = MaterialTheme.colorScheme.onErrorContainer, text = MaterialTheme.colorScheme.errorContainer)
+        ColorChip(label = "outline", color = MaterialTheme.colorScheme.outline, text = MaterialTheme.colorScheme.outlineVariant)
+        ColorChip(label = "outlineVariant", color = MaterialTheme.colorScheme.outlineVariant, text = MaterialTheme.colorScheme.outline)
+        ColorChip(label = "scrim", color = MaterialTheme.colorScheme.onSurface)
+        // 각 컴포즈 컨트롤러와 사용하는 기본 컬러 스킴 값 표시
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // Primary Color 예시
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = null)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text("Primary Button")
+                }
+            },
+            colorSchemeLabel = "Primary"
+        )
+
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // Secondary Color 예시
+                Button(
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Icon(imageVector = Icons.Default.Send, contentDescription = null)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text("Secondary Button")
+                }
+            },
+            colorSchemeLabel = "Secondary"
+        )
+
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // Surface Color 예시
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                    ) {
+                        Icon(imageVector = Icons.Default.Settings, contentDescription = null)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text("Surface Card")
+                    }
+                }
+            },
+            colorSchemeLabel = "Surface"
+        )
+
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // Tertiary Container Color 예시
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = "Tertiary Container",
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
+            },
+            colorSchemeLabel = "Tertiary Container"
+        )
+
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // Error Container Color 예시
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .padding(8.dp)
+                ) {
+                    Text(
+                        text = "Error Container",
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
+            },
+            colorSchemeLabel = "Error Container"
+        )
+
+        ControllerWithColorSchemeLabel(
+            controller = {
+                // TextField with Outline Color 예시
+                var textValue by remember { mutableStateOf(TextFieldValue()) }
+                OutlinedTextField(
+                    value = textValue,
+                    onValueChange = { textValue = it },
+                    label = { Text("Outlined TextField") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    shape = MaterialTheme.shapes.small
+                )
+            },
+            colorSchemeLabel = "TextField with Outline"
+        )
+    }
+}
+
+fun colorToHexString(color: Color): String {
+    // Color을 ARGB 형식의 정수로 변환
+    val argb = android.graphics.Color.argb(
+        (color.alpha * 255).toInt(),
+        (color.red * 255).toInt(),
+        (color.green * 255).toInt(),
+        (color.blue * 255).toInt()
     )
+
+    // 정수를 16진수 문자열로 변환
+    val hexString = Integer.toHexString(argb)
+
+    // "0x"를 앞에 붙여서 반환
+    return "0x$hexString".uppercase()
+}
+
+@Composable
+fun ColorChip(label: String, color: Color, text: Color = MaterialTheme.colorScheme.onPrimary) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color)
+            .padding(8.dp)
+    ) {
+        Text(
+            text = label,
+            color = text,
+            textAlign = TextAlign.Start,
+            style = LocalTextStyle.current,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .fillMaxWidth()
+                .padding(4.dp)
+        )
+        Text(
+            text = "(${colorToHexString(color)})",
+            color = text,
+            textAlign = TextAlign.End,
+            style = MaterialTheme.typography.labelMedium,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .fillMaxWidth()
+                .padding(4.dp)
+        )
+    }
+}
+
+@Composable
+fun ControllerWithColorSchemeLabel(
+    controller: @Composable () -> Unit,
+    colorSchemeLabel: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        // 컨트롤러 위에 컬러 스킴 값 표시
+        Text(
+            text = colorSchemeLabel,
+            style = MaterialTheme.typography.labelMedium,
+            //modifier = Modifier.padding(8.dp)
+        )
+
+        // 실제 컨트롤러
+        controller()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewAppContent() {
     AndTheme {
-        Greeting("Android")
+        // 앱 UI 구성
+        AppContent()
     }
 }
