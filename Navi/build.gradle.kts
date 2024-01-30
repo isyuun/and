962f8e6d8d3b52aef809fb12942d04348db7901c
isyuun-care-pet <isyuun@care-biz.co.kr>
2024-01-30 18:01:41 +0900

    Navi: 버전 5

--- Navi/build.gradle.kts
+++ Navi/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "net.pettip.app.navi"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
